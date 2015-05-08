79f93c434226fe48c0627c96eedecf56acb18615
akettmann <akettmann@gmail.com>
2015-05-08 13:49:17 -0500

    Added test to ensure the machine has virtualization options before proceeding with the download.

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Test if the system has virtualization
+if [[ "$(egrep -c svm\|vmx /proc/cpuinfo)" -gt "0" ]]
+	then
+		echo "Virtualization detected. Proceeding"
+	else 
+		echo "No Virtualization options detected!"
+		echo "Panamax requires virtualization options to function. Please attempt on a machine with VT-x or AMD-V available."
+		exit 1
+fi 
+
 installer='panamax-latest.tar.gz'
 destination=~/.panamax
 curl -O "http://download.panamax.io/installer/$installer"
